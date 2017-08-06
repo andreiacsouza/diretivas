fb816b037f93fa71c7b4e7d8802e2b5f7d4a4436
Andréia de Souza Ohe <andreiadesouzaohe@Andreias-MacBook-Air.local>
2017-08-05 19:10:41 -0700

    my changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaNgifComponent,
+    DiretivaNgswitchComponent,
+    DiretivaNgforComponent,
+    DiretivaNgclassComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
